a756c5d272a5000a2f9c96e4cf7f7bdae25b46d8
Urveshhhh <145110848+Urveshhhh@users.noreply.github.com>
2023-11-13 21:35:20 +0530

    Add files via upload

---
+++ ARRAY.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i, mul[5];
+ for(i=1; i<=5; i++){
+  printf("enter number ");
+  scanf("%d",&[i]);
+  }
+  for(i=0; i<5; i++){
+  printf("number=%d",mul[5]);
+  }
+  getch();
+}
