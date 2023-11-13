b6e65fb662abbd66dc4aa18809567f0ec6657948
Urveshhhh <145110848+Urveshhhh@users.noreply.github.com>
2023-11-13 21:44:03 +0530

    Update and rename MAX3.C to MAX3NESTEDIF.C

--- MAX3NESTEDIF.C
+++ MAX3NESTEDIF.C
@@ -4,13 +4,17 @@ void main(){
  int a,b,c;
  printf("enter value of a b");
  scanf("%d %d",&a,&b);
+
  if(a>b)
  {
   if(a>c){
+
  printf("a is max");}
  else{
+
  printf("c is max");}
  else
+
  printf("b is max");
  getch();
  }
