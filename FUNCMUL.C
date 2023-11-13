097380ce65aa3046df522901a6351d70608667c9
Urveshhhh <145110848+Urveshhhh@users.noreply.github.com>
2023-11-13 21:41:55 +0530

    Update FUNCMUL.C

--- FUNCMUL.C
+++ FUNCMUL.C
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 int mul(int,int);
+
 void main(){
   int p,q,r;
   printf("enter value a b ")";
   scanf("%d %d", &a, &b);
+
   y=mul(a,b);
   printf("ans= %d", y);
-
-
+  getch();
 }
  int mul(int x, int y){
  int ans;
